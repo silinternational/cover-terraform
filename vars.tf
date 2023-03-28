6623f39f69610990903e7adc0fcc180e9a2c1274
Schparky <3172830+Schparky@users.noreply.github.com>
2023-03-28 11:15:02 +0800

    terraform fmt (whitespace only)

--- vars.tf
+++ vars.tf
@@ -232,16 +232,16 @@ variable "user_welcome_email_ending" {
 
 variable "enable_db_backup" {
   description = "Whether to have a database backup or not"
-  type    = bool
-  default = false
+  type        = bool
+  default     = false
 }
 
 variable "backup_cron_schedule" {
-  default = "21 1 * * ? *"		# Every day at 01:21 UTC
+  default = "21 1 * * ? *" # Every day at 01:21 UTC
 }
 
 variable "backup_notification_events" {
   description = "The names of the backup events that should trigger an email notification"
-  type = list(string)
-  default = ["BACKUP_JOB_STARTED", "BACKUP_JOB_COMPLETED", "BACKUP_JOB_FAILED", "RESTORE_JOB_COMPLETED"]
+  type        = list(string)
+  default     = ["BACKUP_JOB_STARTED", "BACKUP_JOB_COMPLETED", "BACKUP_JOB_FAILED", "RESTORE_JOB_COMPLETED"]
 }
