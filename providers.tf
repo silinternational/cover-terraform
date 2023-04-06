29f1a9b6cf41a26f547fac3a85c686f88d7e0fd4
Schparky <3172830+Schparky@users.noreply.github.com>
2023-04-06 19:09:11 +0800

    add standardization tags for AWS

--- providers.tf
+++ providers.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region     = var.aws_region
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
+
+  default_tags {
+    tags = merge({
+      file       = path.module
+      managed_by = "terraform"
+      workspace  = terraform.workspace
+    }, var.tags)
+  }
 }
 
 provider "cloudflare" {
