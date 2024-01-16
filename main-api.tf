9fdd5a4aa3671a66a0574c960e3195ea3a1cf070
briskt <3172830+briskt@users.noreply.github.com>
2024-01-16 15:58:01 -0700

    import SES email identity

--- main-api.tf
+++ main-api.tf
@@ -305,3 +305,14 @@ module "backup_rds" {
   notification_events  = var.backup_notification_events
   sns_topic_arn        = var.backup_sns_topic_arn
 }
+
+/*
+ * AWS SES email identity
+ */
+resource "aws_ses_email_identity" "this" {
+  email = var.email_from_address
+}
+import {
+  id = var.email_from_address
+  to = aws_ses_email_identity.this
+}
